e8c943a1f09ae0ec928187c56ab9cd291b9c4cf9
raghugr27 <raghutheperfect@gmail.com>
2026-01-04 21:34:01 +0530

    js basics pratice array prblm  day4 04jan2026

---
+++ foundation/array/plusOne.js
@@ -0,0 +1,10 @@
+var plusOne = function(digits) {
+    for(let i=digits.length-1;i>=0;i--){
+        digits[i]+=1;
+        if(digits[i]<10) return digits;
+        digits[i]=0;
+    }
+    digits.unshift(1);
+    return digits;
+};
+console.info(plusOne([4,3,2,1]))
