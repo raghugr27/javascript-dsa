8bbc966813f119c20d67d30453f01332999c3d8f
raghugr27 <raghutheperfect@gmail.com>
2026-01-08 23:26:21 +0530

    js basics promise time limit  day8 08jan2026

---
+++ foundation/promise-with-time/promiseTimeLimit.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        const timerPromise = new Promise((_, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+        });
+        return Promise.race([fn(...args), timerPromise]);
+    };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
