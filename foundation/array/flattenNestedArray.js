33678dd05ab71b66889f061b76ebe2fdb06cba4c
raghugr27 <raghutheperfect@gmail.com>
2026-01-17 22:44:13 +0530

    js basics flatten nested array  day17 17jan2026

---
+++ foundation/array/flattenNestedArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} n
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if (n === 0) return arr;
+    
+    const result = [];
+    for (let item of arr) {
+        if (Array.isArray(item) && n > 0) {
+            result.push(...flat(item, n - 1));
+        } else {
+            result.push(item);
+        }
+    }
+    return result;
+};
